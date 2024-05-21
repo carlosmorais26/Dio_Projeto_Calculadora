51a17bd05206b1506a65ce7e79f02b376a77d547
carlos morais <cadu_morais26@hotmail.com>
2024-05-21 17:18:33 -0300

    projeto calculadora

---
+++ src/components/Input/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAFFAA;
+    
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: Roboto;
+    
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #000000;
+        font-size: 24px;
+        padding: 0 10px;
+        font-family: Roboto;
+        border: 0;
+        color: #FFFAFA;
+     
+       
+
+    }
+`
